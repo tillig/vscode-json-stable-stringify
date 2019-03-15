eff40297cfade6313caa02257677f4aab4d39e65
Travis Illig <tillig@paraesthesia.com>
2019-03-15 14:24:40 -0700

    Added some TODO/links

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,11 @@ export function activate(context: vscode.ExtensionContext) {
     console.log('[vscode-json-stable-stringify] activated!');
 
     let disposable = vscode.commands.registerCommand('extension.jsonStableSort', () => {
-        // TODO: If the document isn't JSON, don't allow the command.
-        // TODO: If there is a selection, execute the stringify on the selection.
+        // https://code.visualstudio.com/api/references/vscode-api
+        // languages.registerDocumentRangeFormattingEditProvider('json', provider)
+        // provider = provideDocumentRangeFormattingEdits(document: TextDocument, range: Range, options: FormattingOptions, token: CancellationToken): ProviderResult<TextEdit[]>
+        // TODO: Register a formatter for JSON documents - sections and entire docs.
+        // TODO: Register a stable sort command for invoking the format on a selection in some other doc type.
         // TODO: If there is no selection, execute the stringify on the whole document.
         // The code you place here will be executed every time your command is executed
 

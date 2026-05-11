975a1fe80db2b6eff1dea600ebd117d8208648bb
Travis Illig <tillig@paraesthesia.com>
2026-05-11 09:00:25 -0700

    Update remaining packages.

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ const config = {
             loader: 'ts-loader',
             options: {
               compilerOptions: {
-                module: 'es6' // override `tsconfig.json` so that TypeScript emits native JavaScript modules.
+                module: 'nodenext'
               }
             }
           }
